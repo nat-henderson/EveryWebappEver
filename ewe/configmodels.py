c11e26244a18b216379670f10f4b2ffc8323a99c
Nathan McKinley <ndm25@case.edu>
2013-11-09 06:56:41 +0000

    indirection++

--- ewe/configmodels.py
+++ ewe/configmodels.py
@@ -6,5 +6,18 @@ configengine = create_engine('sqlite:////tmp/config.db', echo=True)
 Base = declarative_base(bind=configengine)
 
 class DBTable(Base):
+    __tablename__ = 'dbtables'
+
+    id = Column(Integer, primary_key = True)
     name = Column(String(255))
     database_table = Column(String(255))
+    references = relationship("DBReference", backref = "from_table")
+
+class DBReference(Base):
+    __tablename__ = 'dbbackrefs'
+
+    id = Column(Integer, primary_key = True)
+    to_table = Column(String(255))
+    to_field = Column(String(255))
+    from_field = Column(String(255))
+    from_table_id = Column(Integer, ForeignKey('dbtables.id'))
